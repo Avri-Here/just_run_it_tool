eac0e6ecb836c2175b8f89fea2d3a68585eb99e8
Avri ! <yoti1492@gmail.com>
2024-10-30 15:02:07 +0200

    disableDragEvents

--- src/app/shortcuts.js
+++ src/app/shortcuts.js
@@ -8,13 +8,8 @@ const registerShortcuts = (mainWindow, xAxis, yAxis,) => {
     globalShortcut.register('Control+2', () => {
         
         mainWindow.setPosition(xAxis, yAxis);
-        mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
+        mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show() && mainWindow.focus();
     });
-
-    globalShortcut.register('Control+3', () => {
-
-        mainWindow.focus();
-        mainWindow.show();
 
     });
 }
